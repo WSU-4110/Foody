dd2eb14b952aed3b8a279b388aebfc9ec722c772
Jordan Tran <jordan22tran@gmail.com>
2021-10-13 10:29:23 -0400

    Created custom hook, find user location
    Locate user location from browser, will use coordinates to locate restaurants near the user

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,19 +1,15 @@
-// import React from 'react'
 import Navigation from './Navigation'
 import React, { useState, useEffect } from 'react';
 import ResturantTab from './ResturantTab';
 import Footer from './Footer';
 import { BEARER_TOKEN } from './config/config';
-
+import useUserCoordinates from './hooks/useUserCoordinates';
 
 const HomePage = () => {
-    const BING_API_URL = ''
-    // Put api key into BEARER_TOKEN const
-    // const BEARER_TOKEN = ''
     const [restaurants, setRestaurants] = useState([])
     const [coordinates, setCoordinates] = useState([])
-
-    // const [coordinateString, setCoordinateString] = useState('')
+    const coord = useUserCoordinates();
+    console.log(coord);
 
     const bingApiRequest = async () => {
         const res = await fetch(`https://dev.virtualearth.net/REST/v1/LocalSearch/?query=restaurant&key=${BEARER_TOKEN}`)
@@ -21,10 +17,10 @@ const HomePage = () => {
         console.log(data)
         setRestaurants(data.resourceSets[0].resources)
         console.log(restaurants)
-   
     }
 
     useEffect(() => {
+
         try {
             bingApiRequest()
         } catch (e) {
