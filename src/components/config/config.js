8773e9816aa46a278367de3ef8d0f1bf9ea7693f
jordantran22 <jordan22tran@gmail.com>
2021-10-12 21:55:08 -0400

    Added ignore files

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,3 +1,3 @@
 // Temporarily leaving API key in this config file
 // We will move this API request to server side code eventually
-export const BEARER_TOKEN = ''; // put your api key here 
+export const BEARER_TOKEN = 'dasdawda'; // put your api key here 
