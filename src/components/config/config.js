6930bcd199a7b3dbc0be843fb9decf421862e629
Mr-Monwe <nnamdimonwe@gmail.com>
2021-11-04 23:26:29 -0400

    Removed config.js

--- src/components/config/config.js
+++
@@ -1,3 +0,0 @@
-// Temporarily leaving API key in this config file
-// We will move this API request to server side code eventually
-export const BEARER_TOKEN = 'dasdawda'; // put your api key here 
