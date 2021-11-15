c2b830f5969b9a84ced3c8f048ffa33646d5fc06
Mr-Monwe <nnamdimonwe@gmail.com>
2021-11-15 12:07:43 -0500

    [FEAT] : Implemented Recaptha without logic

--- src/App.js
+++ src/App.js
@@ -2,41 +2,35 @@ import './App.css';
 import Login from './components/Login'
 import Footer from './components/Footer'
 import Register from './components/Register';
-import {Route, Link} from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import { useState } from 'react';
 import HomePage from './components/HomePage';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 
 function App() {
 
   const [randomBackground, setRandomBackground] = useState('')
 
   const getRandomBackground = (backgroundImageClass) => {
-    const randomBackground = backgroundImageClass[Math.floor(Math.random()*backgroundImageClass.length)]
+    const randomBackground = backgroundImageClass[Math.floor(Math.random() * backgroundImageClass.length)]
     setRandomBackground(randomBackground)
     console.log(randomBackground);
-}
+  }
 
-const backgroundImageClass = ['login-background-1', 'login-background-2', 'login-background-3']
+  const backgroundImageClass = ['login-background-1', 'login-background-2', 'login-background-3']
 
-useEffect(() => {
+  useEffect(() => {
 
-  getRandomBackground(backgroundImageClass)
-  
- 
-}, [])
-
+    getRandomBackground(backgroundImageClass)
+  }, [])
 
   return (
     <div className="App">
-        <Route exact path="/" component={() => <Login background={randomBackground} />} />
-        <Route exact path="/components/Register.js" component={() => <Register background={randomBackground} />} />
-        <Route exact path="/components/HomePage.js" component={HomePage} />
-        <Route exact path="/components/Footer.js" component={Footer} />
-
-        {/* <Login /> */}
-
-      
+      <Route exact path="/" component={() => <Login background={randomBackground} />} />
+      <Route exact path="/components/Register.js" component={() => <Register background={randomBackground} />} />
+      <Route exact path="/components/HomePage.js" component={HomePage} />
+      <Route exact path="/components/Footer.js" component={Footer} />
+      {/* <Login /> */}
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,10 @@ const Footer = () => {
             <div class="section-title"> Contact Us! </div>
             <div> Want to learn more about Foody? Contact us through social media! </div>
             <div> You can find out links down below <i class='bx bx-down-arrow-alt' ></i> </div>
-            
             <div className="footer-socials">
                     <a class="social-link" href="#"><i className='bx bxl-github social-icon'></i></a>
                     <a class="social-link" href="#"><i className='bx bxl-linkedin-square social-icon' ></i> </a>
                     <a class="social-link" href="#"><i className='bx bx-mail-send social-icon' ></i></a>
-             
             </div>
             <div> Copyright &copy; 2021, Foody </div>
         </footer>
