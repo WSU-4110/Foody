18b87f4c8f50441e227f0e6280a214d7be912cc7
Sviatoslav Ivaskiv <hc7607@wayne.edu>
2021-11-16 17:43:47 -0500

    SQL updated to match naming convention

--- dbchanges/reviews.sql
+++ dbchanges/reviews.sql
@@ -1,41 +1,41 @@
 USE foody;
 
-DROP TABLE IF EXISTS foody.restaurants;
-CREATE TABLE foody.restaurants (
-  restaurantId int AUTO_INCREMENT PRIMARY KEY,
-  restaurantName varchar(200),
-  restaurantAddress varchar(200),
-  restaurantPhone varchar(200),
-  restaurantWebsite varchar(200),
-  postDate datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()
+DROP TABLE IF EXISTS foody.restaurant;
+CREATE TABLE foody.restaurant (
+  restaurant_id int AUTO_INCREMENT PRIMARY KEY,
+  restaurant_name varchar(200),
+  restaurant_address varchar(200),
+  restaurant_phone varchar(200),
+  restaurant_website varchar(200),
+  post_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()
 );
 
 
-DROP TABLE IF EXISTS foody.restaurantReviews;
-CREATE TABLE foody.restaurantReviews (
-  reviewId int AUTO_INCREMENT PRIMARY KEY,
-  restaurantId int NOT NULL,
-  userId int NOT NULL,
+DROP TABLE IF EXISTS foody.restaurant_review;
+CREATE TABLE foody.restaurant_review (
+  review_id int AUTO_INCREMENT PRIMARY KEY,
+  restaurant_id int NOT NULL,
+  user_id int NOT NULL,
   review varchar(500) NOT NULL,
-  deliciousnessScore int,
-  serviceScore int,
-  experienceScore int,
-  pricingScore int,
-  pricingValue float,
-  postDate datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  updateDate datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()
+  deliciousness_score int,
+  service_score int,
+  experience_score int,
+  pricing_score int,
+  pricing_value float,
+  post_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  update_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()
 )
 
-DROP TABLE IF EXISTS foody.restaurantImages;
-CREATE TABLE foody.restaurantImages (
-  restaurantId int NOT NULL,
-  reviewId int NOT NULL,
-  imageId int AUTO_INCREMENT PRIMARY KEY,
-  imageName varchar(200),
-  imageSize varchar(50),
-  imageType varchar(50),
-  imageEncoded TEXT CHARACTER SET ascii COLLATE ascii_bin,
-  postDate datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()
+DROP TABLE IF EXISTS foody.review_image;
+CREATE TABLE foody.review_image (
+  restaurant_id int NOT NULL,
+  review_id int NOT NULL,
+  image_id int AUTO_INCREMENT PRIMARY KEY,
+  image_name varchar(200),
+  image_size varchar(50),
+  image_type varchar(50),
+  image_encoded TEXT CHARACTER SET ascii COLLATE ascii_bin,
+  post_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()
 );
 
 
@@ -43,104 +43,105 @@ CREATE TABLE foody.restaurantImages (
 
 -- ------------------------------------------------------------
 
-DROP PROCEDURE IF EXISTS foody.sp_validateRestaurant;
+DROP PROCEDURE IF EXISTS foody.sp_validate_restaurant;
 DELIMITER $$
 
-CREATE PROCEDURE foody.sp_validateRestaurant (
-  IN p_restaurantName varchar(200),
-  IN p_restaurantAddress varchar(200),
-  IN p_restaurantPhone varchar(200),
-  IN p_restaurantWebsite varchar(200)
+CREATE PROCEDURE foody.sp_validate_restaurant (
+  IN p_restaurant_name varchar(200),
+  IN p_restaurant_address varchar(200),
+  IN p_restaurant_phone varchar(200),
+  IN p_restaurant_website varchar(200)
 )
 BEGIN
-  -- SAMPLE RUN -> CALL foody.sp_validateRestaurant ('testRestaurant', '123 test dr', 'xxx-xxx-xxxx', 'http://test.com');
-  INSERT INTO foody.restaurants (restaurantName, restaurantAddress, restaurantPhone, restaurantWebsite)
-  SELECT restaurantName, restaurantAddress, restaurantPhone, restaurantWebsite 
-  FROM (SELECT p_restaurantName, p_restaurantAddress, p_restaurantPhone, p_restaurantWebsite) AS tmp
-  WHERE NOT EXISTS (SELECT 1 FROM foody.restaurants WHERE restaurantName = p_restaurantName AND
-    restaurantAddress = p_restaurantAddress);
+  -- SAMPLE RUN -> CALL foody.sp_validate_restaurant ('testRestaurant', '123 test dr', 'xxx-xxx-xxxx', 'http://test.com');
+  INSERT INTO foody.restaurant (restaurant_name, restaurant_address, restaurant_phone, restaurant_website)
+  SELECT restaurant_name, restaurant_address, restaurant_phone, restaurant_website 
+  FROM (SELECT p_restaurant_name AS restaurant_name, p_restaurant_address AS restaurant_address, 
+    p_restaurant_phone AS restaurant_phone, p_restaurant_website AS restaurant_website) AS tmp
+  WHERE NOT EXISTS (SELECT 1 FROM foody.restaurant WHERE restaurant_name = p_restaurant_name AND
+    restaurant_address = p_restaurant_address);
 
-  SELECT restaurantId from foody.restaurants WHERE restaurantName = p_restaurantName AND restaurantAddress = p_restaurantAddress;
+  SELECT restaurant_id from foody.restaurant WHERE restaurant_name = p_restaurant_name AND restaurant_address = p_restaurant_address;
 END $$
 
 DELIMITER ;
 
 -- --------------------
-DROP PROCEDURE IF EXISTS foody.sp_saveReview;
+DROP PROCEDURE IF EXISTS foody.sp_save_review;
 DELIMITER $$
 
-CREATE PROCEDURE foody.sp_saveReview (
-  IN p_restaurantId int,
-  IN p_userId int,
+CREATE PROCEDURE foody.sp_save_review (
+  IN p_restaurant_id int,
+  IN p_user_id int,
   IN p_review varchar(500),
-  IN p_deliciousnessScore int,
-  IN p_serviceScore int,
-  IN p_experienceScore int,
-  IN p_pricingScore int,
-  IN p_pricingValue float
+  IN p_deliciousness_score int,
+  IN p_service_score int,
+  IN p_experience_score int,
+  IN p_pricing_score int,
+  IN p_pricing_value float
 )
 BEGIN
-  -- SAMPLE RUN -> CALL foody.sp_saveReview (1, 2, 'test', 5, 5, 5, 5, 23.5);
-  IF EXISTS (SELECT 1 FROM foody.restaurantReviews WHERE restaurantId = p_restaurantId AND userId = p_userId)
+  -- SAMPLE RUN -> CALL foody.sp_save_review (1, 2, 'test', 5, 5, 5, 5, 23.5);
+  IF EXISTS (SELECT 1 FROM foody.restaurant_review WHERE restaurant_id = p_restaurant_id AND user_id = p_user_id)
   THEN BEGIN
-    UPDATE foody.restaurantReviews SET
+    UPDATE foody.restaurant_review SET
       review = p_review,
-      deliciousnessScore = p_deliciousnessScore,
-      serviceScore = p_serviceScore,
-      experienceScore = p_experienceScore,
-      pricingScore = p_pricingScore,
-      pricingValue = p_pricingValue,
-      updateDate = CURRENT_TIMESTAMP()
-    WHERE restaurantId = p_restaurantId AND userId = p_userId;
+      deliciousness_score = p_deliciousness_score,
+      service_score = p_service_score,
+      experience_score = p_experience_score,
+      pricing_score = p_pricing_score,
+      pricing_value = p_pricing_value,
+      update_date = CURRENT_TIMESTAMP()
+    WHERE restaurant_id = p_restaurant_id AND user_id = p_user_id;
 
-    SELECT MAX(ReviewId) AS result FROM foody.restaurantReviews WHERE restaurantId = p_restaurantId AND userId = p_userId;
+    SELECT MAX(review_id) AS result FROM foody.restaurant_review WHERE restaurant_id = p_restaurant_id AND user_id = p_user_id;
   END;
   ELSE
-  INSERT INTO foody.restaurantReviews (
-    restaurantId,
-    userId,
+  INSERT INTO foody.restaurant_review (
+    restaurant_id,
+    user_id,
     review,
-    deliciousnessScore,
-    serviceScore,
-    experienceScore,
-    pricingScore,
-    pricingValue
+    deliciousness_score,
+    service_score,
+    experience_score,
+    pricing_score,
+    pricing_value
   )
   VALUES (
-    p_restaurantId,
-    p_userId,
+    p_restaurant_id,
+    p_user_id,
     p_review,
-    p_deliciousnessScore,
-    p_serviceScore,
-    p_experienceScore,
-    p_pricingScore,
-    p_pricingValue
+    p_deliciousness_score,
+    p_service_score,
+    p_experience_score,
+    p_pricing_score,
+    p_pricing_value
   );
 
-  SELECT MAX(ReviewId) AS result FROM foody.restaurantReviews WHERE restaurantId = p_restaurantId AND userId = p_userId;
+  SELECT MAX(review_id) AS result FROM foody.restaurant_review WHERE restaurant_id = p_restaurant_id AND user_id = p_user_id;
   END IF;
 END $$
 
 DELIMITER ;
 
 -- --------------------
-DROP PROCEDURE IF EXISTS foody.calcRestaurantAvgScore;
+DROP PROCEDURE IF EXISTS foody.calc_avg_score;
 DELIMITER $$
 
-CREATE PROCEDURE foody.calcRestaurantAvgScore (
-  IN p_restaurantId int
+CREATE PROCEDURE foody.calc_avg_score (
+  IN p_restaurant_id int
 )
 BEGIN
-  -- SAMPLE RUN -> CALL foody.calcRestaurantAvgScore (0);
+  -- SAMPLE RUN -> CALL foody.calc_avg_score (0);
   SELECT CASE
     WHEN ROUND((
-  (SUM(deliciousnessScore) + 
-  SUM(serviceScore) +
-  SUM(experienceScore) +
-  SUM(pricingScore))/4), 2) = null
+  (SUM(deliciousness_score) + 
+  SUM(service_score) +
+  SUM(experience_score) +
+  SUM(pricing_score))/4), 2) = null
     THEN 0
   END as result
-FROM foody.restaurantreviews WHERE restaurantId = p_restaurantId;
+FROM foody.restaurant_review WHERE restaurant_id = p_restaurant_id;
 END $$
 
 DELIMITER ;
