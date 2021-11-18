865c3dc48bdda05d127c165cd864aa79f75cef6a
Mr-Monwe <nnamdimonwe@gmail.com>
2021-11-18 02:30:57 -0500

    Singleton Design Pattern

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,37 +1,53 @@
 // CSC4110 Assignment: 4
 // Author: Nnamdi Monwe 
 // Date: 11/18/2021 
-// Logout component that renders a button that when clicked,
+// Logout Class that renders a button that when clicked,
 // clears the token and redirects the user to the homepage 
 // Uses singleton design pattern,as there is only one instance of the Logout component.
  
-import React from 'react'
-import  {Link } from 'react-router-dom'
-import { useState, useEffect } from 'react';
-import { useHistory } from 'react-router'
-import Footer from './Footer';
-import { useMediaQuery } from 'react-responsive'
+import React from 'react' 
+ 
+class Logout extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            token: null,
+            username: null,
+            password: null,
+            invalidUsername: false
+        };
+        this.logout = this.logout.bind(this);
+    } 
+    static getInstance() {
+      if (!Logout.instance) {
+        Logout.instance = new Logout();
+      }
+      
+      return Logout.instance;
 
-const Logout = () => {
-    const [token, setToken] = useState(null);
-    const [username, setUsername] = useState(null);
-    const [password, setPassword] = useState(null);
-    const [invalidUsername, setInvalidUsername] = useState(false);
+    }
+    componentDidMount() {
+        this.setState({
+            token: localStorage.getItem("token"),
+            username: localStorage.getItem("username"),
+            password: localStorage.getItem("password")
+        });
+    }
 
-    const logout = () => {
-        setToken(null);
-        setUsername(null);
-        setPassword(null);
-        setInvalidUsername(false);
+    logout() {
+        localStorage.removeItem("token");
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
         window.location.href = "/";
-    };
+    }
 
-    return (
-        <div>
-            <button onClick={logout}>Logout</button>
-        </div>
-    );
+    render() {
+        return (
+            <div>
+                <button onClick={this.logout}>Logout</button>
+            </div>
+        );
+    }
 }
 
 export default Logout;
-         
