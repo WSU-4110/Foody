4856f3a392ab3e79c33f41e7a0d6bb45ae8992ae
TNJRAH <gn8128@wayne.edu>
2021-11-22 21:42:58 -0500

    Update Profile

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -2,9 +2,14 @@ import Navigation from './Navigation'
 import React, { useState, useEffect } from 'react';
 import RecentReviews from './RecentReviews';
 import Footer from './Footer';
+import Login from './Login';
+
 
 
 const ProfilePage = () => {
+
+    const[email, setEmail] = useState('')
+    const[username, setUsername] = useState('')
 
     return (
         <div class="profile-page">
@@ -12,13 +17,30 @@ const ProfilePage = () => {
 
             <div className="profile-page-main-container">
                 
-                <h2>Hello, "Username"!</h2>
+                <h2>Hello, Username!</h2>
 
                 <img className="profile-pic" src="/images/profile-default.svg" alt=""/>
+                <div className="update-container">
+                        
+                        <div className="update-form">
+                            <label> Username </label>
+                            <input className="show-info-text" type="text" placeholder="Username" value={email} ></input>
 
+                            <label> Email</label>
+                            <input className="show-info-text" type="text" placeholder="Email" value={username} ></input>
+
+                            <label> Profile Image </label>
+                            <input className="custom-file-upload" type="file"  ></input>
+
+
+                            <input className="update-info-submit-button" type="submit" ></input>
+                        </div>
+                
+                </div>
                 <h2>Review History</h2>
             </div>
 
+           
             <RecentReviews
                 name={"Pizza"}
                 city={"Hazel Park, MI"}
