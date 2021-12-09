1a6964feb3f521d6d10ec83d7ea2a03a2cba8050
TNJRAH <gn8128@wayne.edu>
2021-12-08 23:55:27 -0500

    Unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { formatNavigations } from './links';
+import './App.css';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+describe('App component', () => {
+ test('it displays the icons and names', async () => {
+   render(<App />);
+
+
+   const linkList = await waitFor(() => screen.getByTestId('link-list'));
+   expect(linkList).toBeInTheDocument();
+ });
+});
+
+jest.mock('axios');
+
+const notWorking = [{
+  "id": 1,
+  "name": "Test link 1",
+  "link": "testlink1",
+ }, {
+  "id": 2,
+  "name": "Test link 2",
+  "link": "testlink2",
+ }];
+
+describe('App component', () => {
+
+ test('it displays new links', async () => {
+   axios.get.mockResolvedValue({ data: notWorking });
+   render(<App />);
+
+
+   const linkList = await waitFor(() => screen.findAllByTestId('link-item'));
+   expect(linkList).toHaveLength(2);
+ });
+});
+
+test('it displays links in a row', async () => {
+  axios.get.mockResolvedValue({ data: notWorking });
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+  const linkList = await waitFor(() => screen.findAllByTestId('link-item'));
+  expect(linkList).toHaveLength(3);
 });
---
+++ src/components/link.js
@@ -0,0 +1,3 @@
+export function formatNavigations(navigation) {
+    return '@' + navigation;
+   }
